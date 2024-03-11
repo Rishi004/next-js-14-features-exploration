0ac07b0e86e291626f007e19865b9635b8d89cca
Rishi004 <rishireiko@gmail.com>
2024-03-11 20:47:58 +0530

    link navigation | replace

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
-function page() {
-    return <h1>Hello World</h1>;
-}
+import Link from "next/link";
 
-export default page;
+export default function page() {
+    return (
+        <>
+            <h1>Hello World</h1>
+            <Link href="/blog">Blog</Link>
+            <br />
+            <Link href="/products">Products</Link>
+        </>
+    );
+}
